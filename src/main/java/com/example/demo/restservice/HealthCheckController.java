b8fdbee2fc7740a866b60a457e7be684947a9e68
princemishra5 <50906541+princemishra5@users.noreply.github.com>
2020-05-25 15:02:44 +0530

    Update HealthCheckController.java

--- src/main/java/com/example/demo/restservice/HealthCheckController.java
+++ src/main/java/com/example/demo/restservice/HealthCheckController.java
@@ -2,12 +2,14 @@ package com.example.demo.restservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HealthCheckController {
-    Logger logger = Logger.getLogger(HealthCheckController.class);
+    private static final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
     
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
